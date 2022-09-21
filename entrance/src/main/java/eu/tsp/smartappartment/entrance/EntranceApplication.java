9bcb7fe192de8ad0a70d275492d0e645b608d1d7
liuchang <913778993@qq.com>
2022-09-21 14:49:52 +0200

    add entrance module

---
+++ entrance/src/main/java/eu/tsp/smartappartment/entrance/EntranceApplication.java
@@ -0,0 +1,15 @@
+package eu.tsp.smartappartment.entrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EntranceApplication.class, args);
+    }
+
+}
---
+++ entrance/src/test/java/eu/tsp/smartappartment/entrance/EntranceApplicationTests.java
@@ -0,0 +1,15 @@
+package eu.tsp.smartappartment.entrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
